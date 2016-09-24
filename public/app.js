a34accc27cd0a9049ab3eaf5cd6a9c9ecf761047
Yaling Wu <valence931215@gmail.com>
2016-09-24 06:20:08 -0400

    geocoding worked and request sent

--- public/app.js
+++ public/app.js
@@ -4,15 +4,9 @@ angular.module('tripApp',
     [
     'ui.bootstrap',
     'tripApp.controllers',
-    // '.filters',
     'tripApp.directives',
     // '.services'
     ])
-    // .config( function( $facebookProvider ) {
-    //     $facebookProvider.setAppId('1624541797795153');
-    //     $facebookProvider.setVersion('v2.4');
-    //     $facebookProvider.setPermissions("public_profile,email,user_friends");
-    // })
 
 angular.module('tripApp.controllers', []);
 angular.module('tripApp.directives', []);
--- public/components/userInput/input_controller.js
+++ public/components/userInput/input_controller.js
@@ -1,8 +1,44 @@
 'use strict';
 
 angular.module('tripApp.controllers')
-.controller('inputController', ['$scope','$rootScope',
-    function($scope,$rootScope){
+.controller('inputController', ['$scope','$rootScope','$http',
+    function($scope,$rootScope,$http){
+        var _curLoc = null;
+        var _duration = null;
 
-    
+        $scope.user = {
+            curLoc: function(newLoc) {
+            // Note that newName can be undefined for two reasons:
+            // 1. Because it is called as a getter and thus called with no arguments
+            // 2. Because the property should actually be set to undefined. This happens e.g. if the
+            //    input is invalid
+                return arguments.length ? (_curLoc = newLoc) : _curLoc;
+            },
+            duration: function(newDuration) {
+                return arguments.length ? (_duration = newDuration) : _duration;
+            }
+        };
+        $scope.send = function(){
+            var curloc = $scope.user.curLoc();
+            var durat = $scope.user.duration();
+            console.log("current location: "+$scope.user.curLoc());
+            console.log("current duration: "+$scope.user.duration());
+
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode( { "address": curloc }, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+                    var location = results[0].geometry.location,
+                        lat      = location.lat(),
+                        lng      = location.lng();
+                    console.log("Latitude: " + lat+ "\nLongitude: " + lng);
+                    $http.get('/results?lat='+lat+'&log='+lng+'&hours='+durat).success(function(res) {  
+                        if(res) {
+                        /** Here is where you can display the map after results are obtained from the server */
+                        } 
+                    });
+                }
+            });
+
+
+        };
 }]);
