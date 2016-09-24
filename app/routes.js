10f054520b9d1bd29c13c783fbcfd2e2d4c43403
kamercer <kyle.mercer.km@gmail.com>
2016-09-24 01:13:03 -0400

    index

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,7 @@
 module.exports = function(app){
     
+    //index page
+    app.get('/', function(req, res){
+        res.sendFile('/views/index.html', {root: __dirname + '/../public'});
+    });
 }
